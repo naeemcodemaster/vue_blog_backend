467cc2a62c751494c5a0ae0a9c6f53755ce8e1a4
= <=>
2024-10-13 15:35:57 +0500

    update index.js file for mongodb

--- config/db.js
+++ config/db.js
@@ -2,5 +2,6 @@ const mongoose = require("mongoose");
 
 exports.ConnectDB = async () => {
   await mongoose.connect(process.env.MONGO_URI);
+  console.log(process.env.MONGO_URI);
   console.log("connected");
 };
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config({
   path: "./.env",
 });
+
 const express = require("express");
 const { Authentication } = require("./routes");
 const { ConnectDB } = require("./config/db");
@@ -8,34 +9,58 @@ const morgan = require("morgan");
 const ApiError = require("./utils/ApiError");
 const { errorConverter, errorHandler } = require("./middlewares/error");
 const httpStatus = require("http-status");
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
-const path = require("path");
+const port = process.env.PORT || 8000;
 
-// Middlewares
-const cors = require("cors");
+// Logging the environment variables immediately after dotenv.config
+if (!process.env.MONGO_URI) {
+  console.error(
+    "Error: MONGODB_URI is not defined. Please check your .env file."
+  );
+  process.exit(1);
+}
+
+console.log("MongoDB URI:", process.env.MONGO_URI);
+console.log("Server Port:", port);
+
+// Middleware setup
 app.use("/static", express.static(path.join(__dirname, "./uploads/")));
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-const port = process.env.PORT || 8000;
-ConnectDB();
+// Database connection
+ConnectDB()
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Failed to connect to MongoDB:", error.message);
+    process.exit(1); // Exit the application if the database connection fails
+  });
 
+// Basic route
 app.get("/", (req, res) => {
   res.send("Hi");
 });
 
+// API routes
 app.use("/api/v1", Authentication);
 
-// Util files included here for api error catch
+// Error handling for not found routes
 app.use((req, res, next) => {
   next(new ApiError(httpStatus.NOT_FOUND, "Not Found"));
 });
+
+// Error converters and handlers
 app.use(errorConverter);
 app.use(errorHandler);
 
+// Start the server
 app.listen(port, () => {
-  console.log(`Sever running or port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
