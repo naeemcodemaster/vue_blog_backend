6c713e3910ec9fc28b184bf41752774d6f24b680
= <=>
2024-10-13 15:27:09 +0500

    update bcrypt to bcryptjs

--- models/User.models.js
+++ models/User.models.js
@@ -1,44 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const bcrypt = require('bcrypt');
-const Schema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
+const bcrypt = require("bcryptjs");
+const Schema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    email:{
-        type:String,
-        required:true,
+    email: {
+      type: String,
+      required: true,
     },
-    password:{
-        type:String,
-        required:true,
+    password: {
+      type: String,
+      required: true,
     },
-},{timestamps:true  
-})
-
-
+  },
+  { timestamps: true }
+);
 
 // Password encypted
-Schema.pre('save',async function(next){
-    if(this.isModified('password')){
-        this.password = await bcrypt.hash(this.password,12);
+Schema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 12);
+  }
 
-    }
-
-    next();
-})
-
-
+  next();
+});
 
 // Password Compare
-Schema.methods.comparePassword = async function(password){
-    const isMatch = await bcrypt.compare(password,this.password);
-    return isMatch;
+Schema.methods.comparePassword = async function (password) {
+  const isMatch = await bcrypt.compare(password, this.password);
+  return isMatch;
+};
 
-}
-
-
-const model = mongoose.model("user",Schema);
+const model = mongoose.model("user", Schema);
 
 module.exports = model;
--- models/contact.models.js
+++ models/contact.models.js
@@ -1,25 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Schema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
+const Schema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    email:{
-        type:String,
-        required:true,
+    email: {
+      type: String,
+      required: true,
     },
-    message:{
-        type:String,
-        required:true
+    message: {
+      type: String,
+      required: true,
     },
-    isContact:{
-        type:Boolean,
-        default:false
-    }
-},{
-    timestamps:true
-})
+    isContact: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const model = mongoose.model('contact',Schema);
+const model = mongoose.model("contact", Schema);
 module.exports = model;
